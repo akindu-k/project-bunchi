d657df60f2418dc9ff14ffc36adee477984e8744
akindu-k <akinduk619@gmail.com>
2024-06-05 12:22:44 +0530

    chore: Update TopicBox to display favorite food and sum of two numbers

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './topicbox.css'
 export default function TopicBox() {
+    const topic = "My favoirte food is kottu."
+    const item = " and rice."
+    const a = 3;
+    const b = 5;
+    
   return (
     <div>
         <div className="topicBox">
-            <span className="text">This is the topic.</span>
+            <span className="text">{topic}{item}{a+b}</span>
         </div>
     </div>
   )
