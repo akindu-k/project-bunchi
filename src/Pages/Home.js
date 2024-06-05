14dfd14de68a91f472213fdfc5cdbb4f5cfa7f39
akindu-k <akinduk619@gmail.com>
2024-06-05 13:24:21 +0530

    chore: Update dependencies and add react-router-dom to project

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import TopicBox from '../components/TopicBox'
-import Content from '../components/Content';
+import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
-    <h1>Homepage</h1>
-    <TopicBox/>
-    <Content/>
+      <h1>Home page</h1>
+      <Link to="/About">Go to the about page</Link>
+      <Link to="/Contact">Go to the contact page</Link>
+
     </div>
+
   );
 }
--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import './topicbox.css'
-export default function TopicBox() {
-    const topic = "My favoirte food is kottu."
-    const item = " and rice."
-    const a = 3;
-    const b = 5;
-    
+import React from "react";
+import "./topicbox.css";
+export default function TopicBox(props) {
   return (
     <div>
-        <div className="topicBox">
-            <span className="text">{topic}{item}{a+b}</span>
-        </div>
+      <div className="topicBox">
+        <span className="text">My favorite food is {props.food}. The price is {props.price}</span>
+      {props.children}
+      </div>
     </div>
-  )
+  );
 }
