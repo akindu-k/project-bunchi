150bdc7bbba972339e06049f2a3b4cc36e0e8a97
akindu-k <akinduk619@gmail.com>
2024-06-05 13:46:33 +0530

    chore: Update CSS styles and add counter functionality to App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { useState } from 'react';
 function App() {
+  const [count , setCount] = useState(0);
+  
+  const increment = () => {
+    setCount(count+1);
+  }
+
+  const decrement = () =>{
+    setCount(count-1);
+  }
+
+
   return (
     <Router>
       <div>
+      <span className="title">My counter</span>
+      <p className="subTitle">My count is {count}</p>
+      <button onClick={decrement} className="button">-</button>
+      <button onClick={increment} className="button">+</button>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
@@ -15,6 +30,8 @@ function App() {
         </Routes>
       </div>
     </Router>
+
+    
   );
 }
 
