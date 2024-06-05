84c499b2a12ba80e32589626e1bc512fcfabedb5
akindu-k <akinduk619@gmail.com>
2024-06-05 11:46:16 +0530

    chore: Add PartOne component to App.js for learning purposes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import PartOne from './components/PartOne';
 
 function App() {
   return (
     <div className="App">
       <h1>I am learning react!</h1>
-      
+      <PartOne/>
+      <PartOne/>
+      <PartOne/>
+      <PartOne/>
+
     </div>
   );
 }
---
+++ src/components/PartOne.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function PartOne() {
+  return (<div>
+    <h1>First React Component</h1>
+    </div>
+    );
+}
